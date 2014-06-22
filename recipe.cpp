3e693f42c56f420eb16264a957468f525087d450
quartorz <tsuhaso@gmail.com>
2014-06-22 14:18:23 +0900

    spec8: 仕様8を実装

--- recipe.cpp
+++ recipe.cpp
@@ -2,33 +2,97 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct Recipe{
-	std::string name, url;
+	int id;
+	string name, url;
+
+	int get_id() const
+	{
+		return id;
+	}
+	string &get_name()
+	{
+		return name;
+	}
+	string &get_url()
+	{
+		return url;
+	}
+	const string &get_name() const
+	{
+		return name;
+	}
+	const string &get_url() const
+	{
+		return url;
+	}
+	friend ostream &operator<<(ostream &o, const Recipe &r)
+	{
+		o << r.id+1 << ": " << r.name << ' ' << r.url;
+		return o;
+	}
 };
- 
+
+class User{
+	string name;
+	vector<Recipe> recipes;
+
+public:
+	User() = default;
+	User(const User &) = default;
+	User(const string &user_name)
+		: name(user_name)
+	{
+	}
+	template <class Container>
+	User(const string &user_name, const Container &recipes)
+		: name(user_name)
+		, recipes(begin(recipes), end(recipes))
+	{
+	}
+	void set_name(const string &n)
+	{
+		name = n;
+	}
+	string &get_name()
+	{
+		return name;
+	}
+	const string &get_name() const
+	{
+		return name;
+	}
+	vector<Recipe> &get_recipes()
+	{
+		return recipes;
+	}
+};
+
 int main(int argc, char *argv[]){
   
-	ifstream inStream(argv[1]);
+	ifstream inStream(argv[2]);
   	string fileText,fileURL;;
-  	Recipe inputRecipe[3];
+  	User user(argv[1]);
   	int lineCounter = 0;
 
    	while ( inStream >> fileText >> fileURL ) {
-    	inputRecipe[lineCounter] = { fileText, fileURL };
+    	user.get_recipes().push_back({ lineCounter, fileText, fileURL });
     	lineCounter++;
   	}
 
   	inStream.close();
 
-	if(argc < 3){
+	cout << "ユーザー名: " << user.get_name() << endl;
+	if(argc < 4){
 		for(int id = 0; id < 3 ; id++){
-			cout << id+1 << ": " << inputRecipe[id].name << ' ' << inputRecipe[id].url << endl;
+			cout << user.get_recipes()[id] << endl;
 		}
 	}else{
-		int id = std::atoi(argv[2]) - 1;
-		cout << id+1 << ": " << inputRecipe[id].name << ' ' << inputRecipe[id].url << endl;
+		int id = atoi(argv[3]) - 1;
+		cout << user.get_recipes()[id] << endl;
 	}
     
     return 0 ;
--- tutorial.md
+++ tutorial.md
@@ -3,6 +3,7 @@ tutorial
 
 プログラムの実行手順
 
+`g++ -std=c++11 recipe.cpp`を実行
 terminalにて a.out hoge.txt を 実行
 hoge.txt からテキストを読み込み
 
