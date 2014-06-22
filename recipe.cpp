4a2e0aab710d3da7b0488d6d2a939fc0fb63bfd3
YujiKoayno <yuji.lkoyano.800@gmail.com>
2014-06-22 12:04:10 +0900

    spec 5
    入力テキスト名をコマンドライン引数にする部分は未実装

--- recipe.cpp
+++ recipe.cpp
@@ -1,12 +1,25 @@
 #include <fstream>
+#include <iostream>
+#include <string>
 using namespace std;
  
-int main(){
+int main(int argc, char *argv[]){
   
-	ofstream fs1("recipe-data.txt");
-    fs1 << "オムライス" << endl;
-    fs1 << "親子丼" << endl;
-    fs1 << "杏仁豆腐" << endl;
+	ifstream inStream("recipe-data.txt");
+  	string inputText[3],fileText;;
+  	int lineCounter = 0;
 
+   	while ( getline(inStream, fileText) ) {
+    	inputText[lineCounter] = fileText;
+    	lineCounter++;
+  	}
+
+  	inStream.close();
+
+	ofstream outStream("recipe-data-with-id.txt");
+	for(int id = 0; id < 3 ; id++){
+		outStream << id+1 << ": " << inputText[id] << endl;
+	}
+    
     return 0 ;
 }
--- tutorial.md
+++ tutorial.md
@@ -4,3 +4,5 @@ tutorial
 プログラムの実行手順
 
 terminalにて a.out を 実行
+recipe-data.txt からテキストを読み込み
+recipe-data-with-id.txt として結果を出力
