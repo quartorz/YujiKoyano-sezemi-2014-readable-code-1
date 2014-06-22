d1b6fec7effb6b0493851dcbc6e7524af6e503dc
quartorz <tsuhaso@gmail.com>
2014-06-22 14:49:11 +0900

    spec9: 仕様9を実装

--- recipe.cpp
+++ recipe.cpp
@@ -7,27 +7,34 @@ using namespace std;
 
 int main(int argc, char *argv[]){
   
-	ifstream inStream(argv[2]);
   	string fileText,fileURL;;
-  	User user(argv[1]);
+  	User users[4];
   	int lineCounter = 0;
-
-   	while ( inStream >> fileText >> fileURL ) {
-    	user.get_recipes().push_back({ lineCounter, fileText, fileURL });
-    	lineCounter++;
+  	for(int i = 0; i < 4; i++){
+  		// argv : ./a.out user1 data1.txt user2 data2.txt ... useri datai.txt
+  		//              0     1         2     3         4 ... 2*i+1     2*i+2
+  		users[i].set_name(argv[i * 2 + 1]);
+  		ifstream inStream(argv[i * 2 + 2]);
+	   	while ( inStream >> fileText >> fileURL ) {
+	    	users[i].get_recipes().push_back({ lineCounter, fileText, fileURL });
+	    	lineCounter++;
+	  	}
   	}
 
-  	inStream.close();
+  	int id;
+  	if(argc < 10)
+  		id = -1;
+  	else
+  		id = atoi(argv[9]) - 1;
 
-	cout << "ユーザー名: " << user.get_name() << endl;
-	if(argc < 4){
-		for(int id = 0; id < 3 ; id++){
-			cout << user.get_recipes()[id] << endl;
-		}
-	}else{
-		int id = atoi(argv[3]) - 1;
-		cout << user.get_recipes()[id] << endl;
-	}
+  	for(int i = 0; i < 4; i++){
+  		cout << "ユーザー名: " << users[i].get_name() << endl;
+  		for(int j = 0; j < 3; j++){
+  			if(id == -1 || users[i].get_recipes()[j].get_id() == id)
+  				cout << users[i].get_recipes()[j] << endl;
+  		}
+  		cout << endl;
+  	}
     
     return 0 ;
 }
