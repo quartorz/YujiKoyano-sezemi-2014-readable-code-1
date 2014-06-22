ca48512da7254dbb1a95e11c8ac0ae4fc5c97f30
quartorz <tsuhaso@gmail.com>
2014-06-22 13:42:37 +0900

    spec6: fix indent

--- recipe.cpp
+++ recipe.cpp
@@ -18,14 +18,14 @@ int main(int argc, char *argv[]){
   	inStream.close();
 
 	ofstream outStream("recipe-data-with-id.txt");
-  if(argc < 3){
-  	for(int id = 0; id < 3 ; id++){
-  		outStream << id+1 << ": " << inputText[id] << endl;
-  	}
-  }else{
-    int id = std::atoi(argv[2]) - 1;
-    outStream << id+1 << ": " << inputText[id] << endl;
-  }
+	if(argc < 3){
+		for(int id = 0; id < 3 ; id++){
+			outStream << id+1 << ": " << inputText[id] << endl;
+		}
+	}else{
+		int id = std::atoi(argv[2]) - 1;
+		outStream << id+1 << ": " << inputText[id] << endl;
+	}
     
     return 0 ;
 }
